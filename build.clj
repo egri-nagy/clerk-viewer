1c756899aac0eac79915d61ff741d58d017fff46
Attila Egri-Nagy <egri-nagy@Attilas-Mac-mini.local>
2023-05-18 16:27:27 +0900

    ditching 1.0 from version, just using git revisions

--- build.clj
+++ build.clj
@@ -2,7 +2,7 @@
   (:require [clojure.tools.build.api :as b]))
 
 (def lib 'clerk-viewer)
-(def version (format "1.0.%s" (b/git-count-revs nil)))
+(def version (format "v%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
